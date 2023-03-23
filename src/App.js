67ad714efa8dc053660a03c0e1cb8a54be20ef78
vishambar <vishambardj@yahoo.com>
2023-03-24 01:06:50 +0530

    basic ui done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { fontawesome } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
+
+  let [ToDo, setToDo] = useState([
+    { 'id': 1, 'Title': 'Task 1', 'status': false },
+    { 'id': 2, 'Title': 'Task 2', 'status': false }
+   ]);
+
+  let [task, setTask] = useState('');
+  let [updateData, setUpdateData] = useState('');
+
+  let addTask = () => {
+
+  }
+
+  let deleteTask = (id) => {
+
+  }
+  let markTask = (id) => {
+
+  }
+  let cancelUpdate = () => {
+
+  }
+  let changeTask = (e) => {
+
+  }
+  let toUpdateTask = () => {
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <div>
+        <br></br>
+        <h3>To Do React App</h3>
+        <h6>{ToDo && ToDo.length? '' : 'No Tasks...'}</h6>
+        <h5>{ToDo && ToDo.map((task,index)=>{
+          return (
+            <React.Fragment key ={task.id}>
+              <div className='col task-bg'>
+                <div className={'task.status'? 'done': ''}>
+              <span className='tasknumber'>{index + 1}</span>
+              <span className='taskText'>{task.Title}</span>
+              </div>
+              <div className='icons'>
+                <span ></span>
+                <span ></span>
+                <span ></span>
+              </div>
+              </div>
+
+            </React.Fragment>
+          )
+
+        })
+        }</h5>
+
+        </div>
+
     </div>
   );
 }
